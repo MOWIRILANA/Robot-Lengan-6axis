ad3802e6ca18dd0539351c3112e7715f3369f408
MOWIRILANA <72381444+MOWIRILANA@users.noreply.github.com>
2023-12-28 00:03:51 +0700

    Update esp32terimadata.ino

--- esp32terimadata.ino
+++ esp32terimadata.ino
@@ -1,56 +1,82 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <AsyncTCP.h>
 
-const char* ssid = "ARMTECH";
-const char* password = "inverse6";
-const int server_port = 12345;
+#define ledPin 2
+int id1 = 128414914200000001402111011710911212146991111141014610911710811610597114114971211481401295114101991111101151161141179911614814714814051101171091121211481407110100971141149712114814714875013314867198148135148821484075175175113414810431405100116121112101148147148140210552148137136135148821484075314016014878787874255255255255742552552552557501161489813767410001481161489846;
 
-int ledPin = 2;  // Gantilah dengan pin LED pada ESP32 Anda
+const char *ssid = "ARMTECH";     // Replace with your WiFi SSID
+const char *password = "inverse6"; // Replace with your WiFi password
+const int serverPort = 12345;    // Port to listen on
 
-WiFiServer server(server_port);
+WiFiServer server(serverPort);
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-
-  // Connect to Wi-Fi
+  // Connect to WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
-  Serial.print("ip address: ");
+  Serial.print("Ipaddress: ");
   Serial.println(WiFi.localIP());
-  // Start the server
   server.begin();
-  Serial.printf("Server listening on port %d\n", server_port);
 }
 
 void loop() {
-  WiFiClient client = server.available();
+  client = server.available();
+
   if (client) {
-    Serial.println("Client connected");
+    Serial.println("New client connected");
 
-    // Receive marker IDs from the client
-    String marker_ids = client.readStringUntil('\n');
-    marker_ids.trim();
+    while (client.connected()) {
+      // Check if there is data available
+      if (client.available()) {
+        // Read the message size
+        uint32_t messageSize;
+        size_t size = client.readBytes((uint8_t*)&messageSize, sizeof(messageSize));
 
-    Serial.print("Received Marker IDs: ");
-    Serial.println(marker_ids);
+        if (size == sizeof(messageSize)) {
+          // Read the actual data
+          uint8_t* data = (uint8_t*)malloc(messageSize);
+          size_t bytesRead = client.readBytes(data, messageSize);
 
-    // Check if marker ID is 1, then turn on LED
-    if (marker_ids.toInt() == 1) {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("LED turned ON");
-    } else {
-      digitalWrite(ledPin, LOW);
-      Serial.println("LED turned OFF");
+          if (bytesRead == messageSize) {
+            // Process the received data (assuming it's marker IDs)
+            process_data(data, messageSize);
+          }
+
+          free(data);
+        }
+      }
     }
 
-    // Close the connection
     client.stop();
     Serial.println("Client disconnected");
   }
-  delay(100);
+}
+
+void process_data(uint8_t* data, size_t size) {
+  // Deserialize the data (marker IDs in this example)
+  Serial.print("Received data: ");
+  for (size_t i = 0; i < size; i++) {
+    Serial.print(data[i]);
+    // Tambahkan spasi jika bukan elemen terakhir
+    if (i < size - 1) {
+      Serial.print("");
+    }
+  }
+  Serial.println();
+  // Add your data processing logic here
+  if (size > 0 && data[0] == id1) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED turned ON");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED turned OFF");
+  }
 }
